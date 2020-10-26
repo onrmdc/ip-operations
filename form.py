9bf42da2b8d10f915329efd1a533cb9f154630c0
ONUR MODACI <onurmodaci@gmail.com>
2020-10-26 15:57:51 +0300

    initial commit

---
+++ form.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+
+class UserForm(FlaskForm):
+    input_ip_address = StringField('input_ip_address', validators=[DataRequired(), Length(min=7, max=15)])
